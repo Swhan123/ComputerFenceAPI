cdb16e897dc27abb92d3a21d5a7d806f64c241c6
Swhan123 <seungwoohan0104@gmail.com>
2023-12-29 22:51:32 +0900

    로그인 완성 / 회원가입 강화

--- DBManager.py
+++ DBManager.py
@@ -4,6 +4,30 @@ def register(id, password):
     encText = hashlib.md5(password.encode()).hexdigest()
     con = sqlite3.connect('./DB/user.db')
     cur = con.cursor()
-    cur.execute(f"INSERT INTO user VALUES ('{id}','{encText}','NONE')")
+    cur.execute(f"INSERT INTO user VALUES ('{id}','{encText}')")
     con.commit()
     con.close()
+
+def login(id, password):
+    encText = hashlib.md5(password.encode()).hexdigest()
+    con = sqlite3.connect('./DB/user.db')
+    cur = con.cursor()
+    cur.execute('SELECT * FROM User')
+    user_list = cur.fetchall()
+    id_list = []
+    ps_list = []
+    code_list = []
+    ps_lock = hashlib.sha256(password.encode()).hexdigest()
+
+    for row in user_list:
+        id_list.append(row[0])
+        ps_list.append(row[1])
+
+    if id in id_list:
+        index = id_list.index(id)
+        if ps_list[index] == ps_lock:
+            return "ACCEPT"
+        else:
+            return "DECLINE"
+    else:
+        return "NOID"
--- main.py
+++ main.py
@@ -9,12 +9,20 @@ def home():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-  '''id = request.headers['id']
-  ps = request.headers['password']'''
-  #for API Tester
+  if request.method == 'GET':
+    return render_template('register.html')
+  else:
+    id = request.form['id']
+    ps = request.form['password']
+    DBManager.register(id, ps)
+    return 'SUCCESS'
+
+@app.route('/login', methods=['POST'])
+def login():
   id = request.form['id']
   ps = request.form['password']
-  DBManager.register(id, ps)
-  return '잘~ 실행중이니 걱정 안하셔도 됩니더'
+  check = DBManager.login(id, ps)
+  if check = "ACCEPT":
+    return 
 
 app.run(host='0.0.0.0', port=8080, debug=True)
